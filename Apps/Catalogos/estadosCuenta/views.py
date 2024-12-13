24a55184899da45053fdd010c91e43b1ea07229e
marcel zuniga <alejocrak321zuniga@gmail.com>
2024-12-13 13:56:41 -0600

    Estado Cuenta se creo: post, patch, delete

--- Apps/Catalogos/estadosCuenta/views.py
+++ Apps/Catalogos/estadosCuenta/views.py
@@ -44,3 +44,93 @@ class EstadoCuentaApiView(APIView):
             logger.error(f"Error al recuperar los estados de cuenta: {e}")
             return Response ({"error": "Hubo un problema al recuperar los datos."},
                              status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @swagger_auto_schema(
+        request_body=EstadoCuentaSerializer, responses={201: EstadoCuentaSerializer}
+    )
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = EstadoCuentaSerializer(data=data)
+            if serializer.is_valid():
+                estadoCuenta = serializer.save()
+                logger.info(f"El usuario '{request.user}' creo un nuevo Estado de cuenta con id: '{estadoCuenta.id}'")
+                return Response(
+                    {"message": "Estado de cuenta creado exitosamente.", "data": serializer.data},
+                    status=status.HTTP_201_CREATED
+                )
+            else:
+                logger.warning(f"Errores de validacion: {serializer.errors}")
+                return  Response(
+                    {"errors": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        except Exception as e:
+            logger.error(f"Error interno del servidor: {str(e)}")
+            return Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo mas tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    @swagger_auto_schema(
+        request_body=EstadoCuentaModel, responses={200: EstadoCuentaModel}
+    )
+    def patch(self, request, id=None):
+        try:
+            try:
+                estadoCuenta = EstadoCuentaModel.objects.get(id=id)
+            except EstadoCuentaModel.DoesNotExist:
+                logger.warning(f"El Estado de cuenta con id {id} no existe.")
+                return Response(
+                    {"error": f"El Estado de cuenta con id {id} no existe."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            serializer = EstadoCuentaSerializer(estadoCuenta, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                logger.info(f"El usuario '{request.user}' actualizó el Estado de cuenta con id: {id}")
+                return Response(
+                    {"message": "Estado de cuenta actualizado exitosamente.", "data": serializer.data},
+                    status=status.HTTP_200_OK
+                )
+            else:
+                logger.warning(f"Errores de validación al actualizar el estado de cuenta con id {id}: {serializer.errors}")
+                return Response(
+                    {"errors": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al actualizar el estado de cuenta con id {id}: {str(e)}")
+            return Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo más tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+    @swagger_auto_schema(responses={204: 'No Content'})
+    def delete(self, request, id=None):
+        try:
+            try:
+                estadoCuenta = EstadoCuentaModel.objects.get(id=id)
+            except EstadoCuentaModel.DoesNotExist:
+                logger.warning(f"El Estado de cuenta con id {id} no existe.")
+                return  Response(
+                    {"error": f"El Estado de cuenta con id {id} no existe."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            #Permiso de objeto
+            self.check_object_permissions(request, estadoCuenta)
+            estadoCuenta.delete()
+
+            logger.info(f"El usuario '{request.user}' elimino el Estado de cuenta con id: {id}")
+            return  Response(
+                {"message": f"El Estado de cuenta con Id {id} eliminado exitosamente."},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al intentar eliminar el estado de cuenta con id {id}: {str(e)}"),
+            return Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo más tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
