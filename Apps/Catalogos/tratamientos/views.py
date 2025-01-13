59a119b60d04c3fc496d619a21eee5b96e81243c
marcel zuniga <alejocrak321zuniga@gmail.com>
2025-01-13 09:32:57 -0600

    Tratamiento se creo: patch, delete

--- Apps/Catalogos/tratamientos/views.py
+++ Apps/Catalogos/tratamientos/views.py
@@ -85,3 +85,75 @@ class TratamientoApiView(APIView):
                 },
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+    @swagger_auto_schema(
+        request_body=TratamientoSerializer, responses={200: TratamientoSerializer}
+    )
+    def patch(self, request, id=None):
+        try:
+            try:
+                tratamiento = TratamientoModel.objects.get(id=id)
+            except TratamientoModel.DoesNotExist:
+                logger.warning(f"El Tratamiento con id {id} no existe.")
+                return  Response(
+                    {"error": {
+                        'Id':{
+                            f"El Tratamiento con id {id} no existe."
+                        }
+                    }},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            serializer = TratamientoSerializer(tratamiento, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                logger.info(f"El usuario '{request.data}' actualizo el Tratamiento con id {id}")
+                return Response(
+                    {
+                        "message": "Tratamiento actualizado exitosamente.",
+                        "data": serializer.data,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                logger.warning(f"Errores de validación: {serializer.errors}")
+                return Response(
+                    {"errors": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al actualizar el tratamiento con id {id}: {str(e)}")
+            return  Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo mas tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    @swagger_auto_schema(responses={204: 'No Content'})
+    def delete(self, request, id=None):
+        try:
+            try:
+                tratamiento = TratamientoModel.objects.get(id=id)
+            except TratamientoModel.DoesNotExist:
+                logger.warning(f"El Tratamiento con id {id} no existe.")
+                return  Response(
+                    {"error": {
+                        'Id':{
+                            f"El Tratamiento con id {id} no existe."
+                        }
+                    }},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            self.check_object_permissions(request, tratamiento)
+            tratamiento.delete()
+
+            logger.info(f"El usuario '{request.user}' elimino el Tratamiento con id: {id}")
+            return Response(
+                {"message": f"El Tratamiento con Id {id} eliminado exitosamente."},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al intentar eliminar el Tratamiento con id {id}: {str(e)}"),
+            return Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo más tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
