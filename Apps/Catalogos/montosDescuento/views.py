57d8fb82e004d268fa75cda047c374c6a16858ab
marcel zuniga <alejocrak321zuniga@gmail.com>
2025-01-13 08:07:46 -0600

    MontoDescuento se creo: patch

--- Apps/Catalogos/montosDescuento/views.py
+++ Apps/Catalogos/montosDescuento/views.py
@@ -71,3 +71,40 @@ class MontoDescuentoApiView(APIView):
                 },
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+    @swagger_auto_schema(
+        request_body=MontoDescuentoSerializer, responses={200: MontoDescuentoSerializer}
+    )
+    def patch(self, request, id=None):
+        try:
+            try:
+                montodescuento = MontoDescuentoModel.objects.get(id=id)
+            except MontoDescuentoModel.DoesNotExist:
+                logger.warning(f"El monto de descuento con id {id} no existe.")
+                return  Response(
+                    {"error": f"El Estado cita con id {id} no existe"},
+                    status = status.HTTP_404_NOT_FOUND
+                )
+            serializer = MontoDescuentoSerializer(montodescuento, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                logger.info(f"El usuario '{request.data}' actualizo el monto de descuento con id {id}")
+                return Response(
+                    {
+                        "message": "Monto de descuento actualizado exitosamente.",
+                        "data": serializer.data,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                logger.warning(f"Errores de validación: {serializer.errors}")
+                return Response(
+                    {"errors": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al actualizar el monto de descuento con id {id}: {str(e)}")
+            return  Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo mas tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
