6276594449ea1861ba038c4bca4c99f835a2ec15
marcel zuniga <alejocrak321zuniga@gmail.com>
2025-01-15 08:55:11 -0600

    Metodos del modelo modalidad Terminados.

--- Apps/Catalogos/modalidadesPago/views.py
+++ Apps/Catalogos/modalidadesPago/views.py
@@ -48,3 +48,113 @@ class ModalidadPagoApiView(APIView):
             logger.error(f"Error al recuperar las modalidades: {e}, usuario: {request.user}")
             return Response ({"error": "Hubo un problema al recuperar los datos."},
                              status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @swagger_auto_schema(
+        request_body=ModalidadPagoSerializer, responses={201: ModalidadPagoSerializer}
+    )
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = ModalidadPagoSerializer(data=data)
+
+            if serializer.is_valid():
+                modalidad = serializer.save()
+                logger.info(
+                    f"El usuario '{request.user}' creó una nueva modalidad con id: '{modalidad.id}'"
+                )
+                return Response(
+                    {
+                        "message": "Modalidad creada exitosamente.",
+                        "data": serializer.data,
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                logger.warning(f"Errores de validación: {serializer.errors}, usuario: {request.user}")
+                return Response(
+                    {"errors": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        except Exception as e:
+            # Registra errores en el servidor
+            logger.error(f"Error interno del servidor: {str(e)}, usuario: {request.user}")
+            return Response(
+                {
+                    "error": "Error interno del servidor. Por favor, inténtelo de nuevo más tarde."
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    @swagger_auto_schema(
+        request_body=ModalidadPagoSerializer, responses={200: ModalidadPagoSerializer}
+    )
+    def patch(self, request, id=None):
+        try:
+            try:
+                modalidad = ModalidadPagoModel.objects.get(id=id)
+            except ModalidadPagoModel.DoesNotExist:
+                logger.warning(f"La modalidad con id {id} no existe, usuario: {request.user}")
+                return  Response(
+                    {"error": {
+                        'Id':{
+                            f"La modalidad con id {id} no existe."
+                        }
+                    }},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            serializer = ModalidadPagoSerializer(modalidad, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                logger.info(f"El usuario '{request.user}' actualizo la modalidad con id {id}")
+                return Response(
+                    {
+                        "message": "Modalidad actualizada exitosamente.",
+                        "data": serializer.data,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                logger.warning(f"Errores de validación: {serializer.errors}, usuario: {request.user}")
+                return Response(
+                    {"errors": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al actualizar la modalidad con id {id}: {str(e)}, usuario: {request.user}")
+            return  Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo mas tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+    @swagger_auto_schema(responses={204: 'No Content'})
+    def delete(self, request, id=None):
+        try:
+            try:
+                modalidad = ModalidadPagoModel.objects.get(id=id)
+            except ModalidadPagoModel.DoesNotExist:
+                logger.warning(f"La modalidad con id {id} no existe, usuario: {request.user}")
+                return  Response(
+                    {"error": {
+                        'Id':{
+                            f"La modalidad con id {id} no existe."
+                        }
+                    }},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            self.check_object_permissions(request, modalidad)
+            modalidad.delete()
+
+            logger.info(f"El usuario '{request.user}' elimino la modalidad con id: {id}")
+            return Response(
+                {"message": f"La modalidad con Id {id} eliminado exitosamente."},
+                status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            logger.error(f"Error interno del servidor al intentar eliminar la modalidad con id {id}: {str(e)}, usuario: {request.user}"),
+            return Response(
+                {"error": "Error interno del servidor. Por favor intentelo de nuevo más tarde."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
